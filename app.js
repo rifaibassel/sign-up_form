e64d54b68c075ae205a2a7ed634767bf3ba8a66c
Ahmad Alrefai <bassel@basselalrefai.com>
2023-01-08 13:50:14 -0500

    Add script and css for error text

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const passInputField = document.getElementById('pass');
 const confirmPassInputField = document.getElementById('confirm_pass');
+// const passwordErrorTextDiv = document.createElement('div');
+const passwordErrorText = document.createElement('p');
+passwordErrorText.textContent = '* Passwords do not match';
+passwordErrorText.classList.add('error-text');
 
 console.log(passInputField);
 console.log(confirmPassInputField);
@@ -11,8 +15,10 @@ function passCheckHandler(e) {
   if (passInputField.value !== confirmPassInputField.value) {
     passInputField.classList.add('error');
     confirmPassInputField.classList.add('error');
+    passInputField.parentElement.appendChild(passwordErrorText);
   } else {
     passInputField.classList.remove('error');
     confirmPassInputField.classList.remove('error');
+    passInputField.parentElement.removeChild(passwordErrorText);
   }
 }
